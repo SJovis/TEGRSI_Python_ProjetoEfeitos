bc11b11a17675338e83f5832d6324f04eda9a9f0
SJovis <jovis.sergio@gmail.com>
2025-05-27 19:27:27 +0100

    fixed bug that showed folders at the end of the list

--- src/treep.py
+++ src/treep.py
@@ -7,7 +7,8 @@ Descrição:
     Este script percorre recursivamente uma diretoria e apresenta
     a sua estrutura hierárquica em formato de árvore, com suporte a
     profundidade limitada (-L), exibição de caminhos completos (-f),
-    e listagem apenas de diretórios (-d).
+    listagem apenas de diretórios (-d) e exportação do output para
+    um ficheiro .html (-H).
 """
 
 import os
@@ -32,86 +33,71 @@ total_diretorias = 0
 def main(diretoria: str):
     # Exibe em modo árvore todas as diretorias e ficheiros recursivamente
     mostrar_arvore(diretoria)
+    # Exporta para um ficheiro html
     exportar_para_html(diretoria) if args.html else None
 
 def mostrar_arvore(diretoria: str):
     # Snapshot dos ficheiros presentes na diretoria root
-    _, _, ficheiros_raiz = next(os.walk(diretoria))
+    rootpath, _, rootfiles = next(os.walk(diretoria))
 
     # os.walk() retorna um gerador(comporta-se como um iterador) que percorre de forma recursiva
     # todos as pastas e ficheiros a partir da diretoria fornecida.
     for dirpath, dirnames, _ in os.walk(diretoria):
         # Conta a quantidade de separadores '/' para defenir a profundidade da diretoria atual em relação à root
         depth = dirpath[len(diretoria):].count(os.sep)
+        is_root = rootpath == dirpath # Verifica se a diretoria corrente é a root
+        
         # Se o nível de recursividade for maior que o args.level a lista diretorias fica vazia impedindo
         # o os.walk() de continuar a descer de níveis.
         if depth > args.level:
             dirnames[:] = [] 
             continue # Passa para a próxima iteração
-
+        
         # Exibe todas as diretorias e os seus ficheiross
-        exibir_diretorias(diretorias=dirnames, depth=depth, dirpath=dirpath)
+        print_diretoria(depth=depth,
+                        dirpath=dirpath,
+                        sub_dir=os.path.basename(dirpath), 
+                        root=is_root)
 
+    # Exibe os ficheiros da diretoria root
     if not args.d:
-        exibir_ficheiros(ficheiros=ficheiros_raiz,depth=0, dirpath=dirpath)
+        exibir_ficheiros(ficheiros=rootfiles,depth=0, dirpath=dirpath)
     print()
     # Mostrar número de diretorias e ficheiros encontrados
     print(f"{total_diretorias} diretorias, {total_ficheiros} ficheiros")
 
 # Todos os prints estão formatados com a indentação ser proporcional ao nível de profundidade
 def exibir_ficheiros(ficheiros : list[str], depth : int, dirpath : str):
+    global total_ficheiros
     for f in ficheiros:
-        global total_ficheiros
         total_ficheiros += 1
         file_indent = '│   ' * (depth) # Indentação da linha
-        file_path = os.path.join(dirpath, f) # Junta o caminho actual com o nome do ficheiro para obter o caminho do ficheiro
-        #hasPermission = os.access(file_path, os.R_OK) # permissões não estão a funcionar para ficheiros
-        #if hasPermission:
+        # Junta o caminho actual com o nome do ficheiro para obter o caminho do ficheiro
+        file_path = os.path.join(dirpath, f) 
         line = f"{file_indent}└── {f}"
-        # else:
-         #   line = f"{file_indent}└──{Color.RED} {f}{Color.END}"
         if args.f:
             line += f"{Color.CYAN} {file_path}{Color.END}"
         print(line)
 
-
-def exibir_diretorias(diretorias : list[str], depth : int, dirpath : str):
-    global total_diretorias
-    if depth == 0:
-        # Se o nível de profundidade for 0, exibe apenas as diretorias sem ficheiros, 
-        # Não exibe ficheiros para garantir que os ficheiros da root aparecem sempre em último.
-        for sub_dir in diretorias:
-            total_diretorias += 1
-            print_diretoria(depth=depth, dirpath=dirpath, sub_dir=sub_dir)
-    else:
-        # Exibe pastas e ficheiros
-        for sub_dir in diretorias:
-                    total_diretorias += 1
-                    print_diretoria(depth=depth, dirpath=dirpath, sub_dir=sub_dir)
-                    if not args.d:
-                        # Path dos ficheiros da sub-diretoria 
-                        files_path = os.path.join(dirpath,sub_dir)
-                        sub_files = []
-                        # [sub_files.append(f) for f in os.listdir(files_path) if os.path.isfile(os.path.join(files_path,file))]
-                        for file in os.listdir(files_path):
-                            # Todos os ficheiros validados são guardados
-                            if os.path.isfile(os.path.join(files_path,file)):
-                                sub_files.append(file) 
-                        # São exibidos todos os ficheiros das sub-diretorias
-                        exibir_ficheiros(ficheiros=sub_files, depth=depth+1, dirpath=dirpath)
-
 # As diretorias têm o texto a verde e bold e todos os paths/caminhos a ciano 
-def print_diretoria(depth : int, dirpath : str, sub_dir : str):
+def print_diretoria(depth : int, dirpath : str, sub_dir : str, root : bool):
+    global total_diretorias
     indent = '│   ' * depth 
-    hasPermission = os.access(os.path.join(dirpath, sub_dir), os.R_OK)
-    # Vermelho se não tiver permissões
-    if hasPermission:
-        line = f"{indent}├── {Color.BOLD + Color.GREEN}{sub_dir}{Color.END}"
-    else:
-        line = f"{indent}├── {Color.BOLD + Color.RED}{sub_dir}{Color.END}"
+    line = f"{indent}├── {Color.BOLD + Color.GREEN}{sub_dir}{Color.END}"
     if args.f:
-        line += f"{Color.CYAN} {os.path.join(dirpath, sub_dir)}{Color.END}"
+        line += f"{Color.CYAN} {dirpath}{Color.END}"
+    total_diretorias += 1
     print(line)
+
+    if not args.d:
+        ficheiros = []
+        for f in os.listdir(dirpath):
+            # Se o f for avaliado como ficheiro adiciona à lista.
+            if os.path.isfile(os.path.join(dirpath, os.path.basename(f))):
+                ficheiros.append(f)
+        # Se a diretoria a ser avaliada for root não exibe ficheiros
+        if not root:
+            exibir_ficheiros(ficheiros=ficheiros, depth=depth+1, dirpath=dirpath)
 
 # Validação de input e argumentos
 def validations(args,unknown):
@@ -138,12 +124,14 @@ def exportar_para_html(diretoria: str):
     nome_ficheiro = ''.join([diretoria, '_export.html'])
     # Inicializar ficheiro HTML
     html_file = io.StringIO()
+    # A tag <pre> de html permite manter o formato do texto
     html_file.write("<html><head><meta charset='utf-8'><title>TREEP.py</title></head><body>")
     html_file.write(f"<h1>Estrutura de {diretoria}</h1><pre><br>")
 
-    # Redireciona a saída padrão temporariamente para capturar o output da árvore
+    # Redireciona a saída padrão temporariamente para capturar o output da funcção {mostrar_arvore}
     with redirect_stdout(html_file):
         mostrar_arvore(diretoria)
+
     html_file.write("</pre></body></html>")
     
     with open(nome_ficheiro, "w", encoding="utf-8") as f:
